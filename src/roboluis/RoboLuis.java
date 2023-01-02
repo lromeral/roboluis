3b81eb902f93aaeca2cb4eb1d45894742bd42925
lromeral <luis.romeral@gmail.com>
2023-01-03 00:27:32 +0100

    Función de ir a esquina completada

--- src/roboluis/RoboLuis.java
+++ src/roboluis/RoboLuis.java
@@ -4,9 +4,7 @@
  */
 package roboluis;
 import robocode.*;
-import robocode.util.*;
-import robocode.AdvancedRobot;
-import java.util.*;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 /**
  * Robot para asignatura PROGRAMACION
@@ -15,7 +13,7 @@ import java.util.*;
  * 
  * ROBOCODE API: https://robocode.sourceforge.io/docs/robocode/
  */
-public class RoboLuis extends AdvancedRobot {
+public class RoboLuis extends Robot {
     
     double posX;
     double posY;
@@ -26,8 +24,12 @@ public class RoboLuis extends AdvancedRobot {
     double orientacion;
     final static double ROBOT_HEIGHT = 36 * 2;
     final static double ROBOT_WIDTH = 36 * 2;
-    
-    Robot[] enemigos;
+    final static int ESQ_INF_IZDA = 1;
+    final static int ESQ_SUP_IZDA = 2;
+    final static int ESQ_SUP_DCHA = 3;
+    final static int ESQ_INF_DCHA = 4;
+    final static int CENTRO = 0;
+  
     
     public void RoboLuis(){
         posX = 0.0;
@@ -42,26 +44,21 @@ public class RoboLuis extends AdvancedRobot {
 
     @Override
     public void run(){
-        double xToGo = 0;
-        double yToGo = 0;
         
-        while (true){
-            posX = getX();
-            posY = getY();
-            
-            if (posX < ROBOT_WIDTH && posY < ROBOT_HEIGHT){
-                xToGo = tableroAncho;
-                yToGo = tableroAlto;
-            }
-            else if (posX > tableroAncho - ROBOT_WIDTH && posY > tableroAlto + ROBOT_HEIGHT){
-                xToGo = 0;
-                yToGo = 0;
-            }
-            
-            
-            goTo(xToGo,yToGo);
-            execute();
+        //Vamos a nuestra esquina preferida
+          
+        //turnRight(normalRelativeAngleDegrees(corner - getHeading()));
+        //Obtener la posicion donde salimos con getHeading() y luego hay que girarse hasta apuntar a la esquina que queramos.
+        out.println ("Empieza");
+        for (int i=1;i<5;i++){
+            out.println ("ESquina:" + i);
+            irAEsquina (i);
+            fire (1);
         }
+        
+        
+        
+ 
     }
     //Detecta un robot
     @Override
@@ -92,48 +89,93 @@ public class RoboLuis extends AdvancedRobot {
     
     //This method is called when your robot collides with a wall.
     @Override
-    public void onHitWall(HitWallEvent event){}
+    public void onHitWall(HitWallEvent event){
+        out.println ("impacto pared");
+    }
 
     @Override
     //This method is called every turn in a battle round in order to provide the robot status as a complete snapshot of the robot's current state at that specific time.
     public void onStatus(StatusEvent e){
-        //posX = getX();
-        //posY = getY();
+        posX = getX();
+        posY = getY();
         energy = getEnergy();
         gunHeat = getGunHeat();
         orientacion = getHeading();
-        out.println ("CoorX: " + posX + " CoorY: " + posY + " Orientacion: " + orientacion +  " energía: " + energy);
+        
+        out.println ("CoorX: " + posX + " CoorY: " + posY + " Orientacion: " + orientacion +  "O. Norm: " + normalRelativeAngleDegrees(orientacion)    + " energía: " + energy);
     }
     
-/**
- * This method is very verbose to explain how things work.
- * Do not obfuscate/optimize this sample.
- */
-private void goTo(double x, double y) {
-	/* Transform our coordinates into a vector */
-	x -= getX();
-	y -= getY();
-	
-	/* Calculate the angle to the target position */
-	double angleToTarget = Math.atan2(x, y);
-	
-	/* Calculate the turn required get there */
-	double targetAngle = Utils.normalRelativeAngle(angleToTarget - getHeadingRadians());
-	
-	/* 
-	 * The Java Hypot method is a quick way of getting the length
-	 * of a vector. Which in this case is also the distance between
-	 * our robot and the target location.
-	 */
-	double distance = Math.hypot(x, y);
-	
-	/* This is a simple method of performing set front as back */
-	double turnAngle = Math.atan(Math.tan(targetAngle));
-	setTurnRightRadians(turnAngle);
-	if(targetAngle == turnAngle) {
-		setAhead(distance);
-	} else {
-		setBack(distance);
-	}
-}
+    //Se desplaza a una esquina
+    //Las esquinas estan nombradas 1,2,3 y 4 en sentido horario empezando por la inferior izquierda
+    //1 - Esquina inferior Izquierda
+    //2 - Esquina Superior Izquierda
+    //3 - Esquina Superior Derecha
+    //4 - Esquina Inferior Derecha
+    public void irAEsquina (int numEsquina){
+        out.println ("Ir a esquina");
+        switch (numEsquina){
+            case 0:
+                break;
+            case 1:
+                //Girar para encarse a la pared izquierda
+                girarHastaGrado (getHeading(), 270);
+                //Avanzar hasta la pared
+                ahead(getX());
+                //Gira 90 grados a la izquierda
+                turnLeft(90);
+                //Avanza hasta la esquina (0,0)
+                ahead (getY());
+                //Gira 135º para encarse al tablero
+                turnLeft (135);
+                break;
+            case 2:
+                //Girar para encarse a la pared izquierda
+                girarHastaGrado (getHeading(), 270);
+                //Avanzar hasta la pared
+                ahead(getX());
+                //Gira 90 grados a la izquierda
+                turnRight(90);
+                //Avanza hasta la esquina (0,0)
+                ahead (getBattleFieldHeight() - getY());
+                //Gira 135º para encarse al tablero
+                turnRight (135);
+                break;
+            case 3:
+                //Girar para encarse a la pared izquierda
+                girarHastaGrado (getHeading(), 90);
+                //Avanzar hasta la pared
+                ahead(getBattleFieldWidth () - getX());
+                //Gira 90 grados a la izquierda
+                turnLeft(90);
+                //Avanza hasta la esquina (0,0)
+                ahead (getBattleFieldHeight() - getY());
+                //Gira 135º para encarse al tablero
+                turnLeft (135);
+                break;
+            case 4:
+                //Girar para encarse a la pared izquierda
+                girarHastaGrado (getHeading(), 90);
+                //Avanzar hasta la pared
+                ahead(getBattleFieldWidth () - getX());
+                //Gira 90 grados a la izquierda
+                turnRight(90);
+                //Avanza hasta la esquina (0,0)
+                ahead (getY());
+                //Gira 135º para encarse al tablero
+                turnRight (135);
+                break;
+            default:
+                break;
+        }
+    }
+    
+    private void girarHastaGrado (double origen, double destino){
+        if (origen > destino){
+            turnLeft (origen - destino);
+        }else if (origen < destino){
+            turnRight(destino - origen);
+        }else{
+            //Nada
+        }
+    }
 }
